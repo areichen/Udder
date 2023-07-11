c478f1b56bbe8f31c1556f29a08bf05d67439531
Andri Reichenbacher <andrir@greenmail.ch>
2023-07-11 14:09:11 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Reichenbacher Andri (reie) on 11.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
